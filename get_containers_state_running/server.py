d94444c9d90e91e39ebbffd5fbbcb6c3730ed076
John Claro <jkrclaro@gmail.com>
2017-11-30 21:10:33 +0000

    Add

---
+++ get_containers_state_running/server.py
@@ -0,0 +1,40 @@
+import json
+from subprocess import Popen, PIPE
+
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+
+def docker(*args):
+    cmd = ['docker']
+    for sub in args:
+        cmd.append(sub)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    if stderr.startswith('Error'):
+        print 'Error: {0} -> {1}'.format(' '.join(cmd), stderr)
+    return stderr + stdout
+
+
+def docker_ps_to_array(output):
+    all = []
+    for c in [line.split() for line in output.splitlines()[1:]]:
+        each = {}
+        each['id'] = c[0]
+        each['image'] = c[1]
+        each['name'] = c[-1]
+        each['ports'] = c[-2]
+        all.append(each)
+    return all
+
+@app.route('/containers')
+def containers():
+    if request.args.get('state') == 'running':
+        output = docker('ps')
+    else:
+        # Your final assignment should not be returning the below Response
+        return Response(response='You must specify ?state=running')
+    resp = json.dumps(docker_ps_to_array(output))
+    return Response(response=resp, mimetype="application/json")
+
+app.run(port=8080, debug=True)
